a1394d85d9af986b5cbf4d2cb90be0be18911053
Muhammad Aditya Sasongko <msasongko17@ku.edu.tr>
2019-10-21 15:19:58 +0300

    considering only all_store samples in deciding expiration periods

--- src/tool/hpcrun/sample-sources/watchpoint_clients.c
+++ src/tool/hpcrun/sample-sources/watchpoint_clients.c
@@ -2635,7 +2635,7 @@ SET_FS_WP: ReadSharedDataTransactionally(&localSharedData);
 			    uint64_t curtime = rdtsc();
 
 			    int64_t storeCurTime = 0;
-			    if(/*sType == ALL_STORE*/ accessType == STORE || accessType == LOAD_AND_STORE)
+			    if(sType == ALL_STORE /*accessType == STORE || accessType == LOAD_AND_STORE*/)
 			      storeCurTime = curtime;
 
 
@@ -2814,7 +2814,7 @@ SET_FS_WP: ReadSharedDataTransactionally(&localSharedData);
 			    }
 
 			    // if ( A1 is not STORE) or (entry != NULL and M2 has not expired) then
-			    if((accessType == LOAD) || ((item.cacheLineBaseAddress != -1) && (me == item.tid) && ((curtime - item.time) <= (storeCurTime - storeLastTime)))) {
+			    if(/*(accessType == LOAD)*/ (sType == ALL_LOAD)  || ((item.cacheLineBaseAddress != -1) && (me == item.tid) && ((curtime - item.time) <= (storeCurTime - storeLastTime)))) {
 			    } else {
 			      // BulletinBoard.TryAtomicPut(key = L1 , value = < M1 , δ1 , ts1 , T1 >)
 			      uint64_t bulletinCounter = bulletinBoard.counter;
@@ -2844,7 +2844,7 @@ SET_FS_WP: ReadSharedDataTransactionally(&localSharedData);
 			    // ends
 
 			    lastTime = curtime;
-			    if(accessType == STORE || accessType == LOAD_AND_STORE)
+			    if( sType == ALL_STORE  /*accessType == STORE || accessType == LOAD_AND_STORE*/)
 			      storeLastTime = storeCurTime;
 			  }
     default:
