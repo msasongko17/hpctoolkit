517ec14c6c5f73a57b275144a4ccbcfa14816a48
Muhammad Aditya Sasongko <msasongko17@ku.edu.tr>
2020-03-23 14:25:10 +0300

    adding reuse.h

---
+++ src/tool/hpcrun/sample-sources/reuse.h
@@ -0,0 +1,19 @@
+typedef struct reuseBBEntry{
+  uint64_t time __attribute__((aligned(CACHE_LINE_SZ)));
+  int tid;
+  int core_id;
+  AccessType accessType;
+  void *address;
+  void *cacheLineBaseAddress;
+  int accessLen;
+  cct_node_t * node;
+  char dummy[CACHE_LINE_SZ];
+} ReuseBBEntry_t;
+
+typedef struct reuseHashTableStruct{
+  volatile uint64_t counter __attribute__((aligned(64)));
+  struct reuseBBEntry hashTable[503];
+  //struct SharedData * hashTable;
+} ReuseHashTable_t;
+
+extern ReuseHashTable_t reuseBulletinBoard;
