71eaeb33b7db7646c55f422e7d5059c25ed6e8da
msasongko17 <msasongko17@ku.edu.tr>
2021-04-06 13:12:14 +0300

    time reuse distance is wrt. L1 data cache access

--- src/tool/hpcrun/sample-sources/perf/linux_perf.c
+++ src/tool/hpcrun/sample-sources/perf/linux_perf.c
@@ -1023,7 +1023,7 @@ METHOD_FN(process_event_list, int lush_metrics)
 		// ------------------------------------------------------------
 		if(!hpcrun_ev_is(name, "IBS_OP")) {
 			if(hpcrun_ev_is(name, "AMD_L1_DATA_ACCESS")) {
-                        	event_attr->config = /*0x0c0;0x4300c1;*/0x0329;//0x430729;
+                        	event_attr->config = /*0x0c0;0x4300c1;0x0329;*/0x430729;
                         	event_attr->type = PERF_TYPE_RAW;
                 	}
 			perf_attr_init(event_attr, is_period, threshold, 0);
